28e0c82d0e9fddb387888e0b12b903dd6bc856c2
Thong Nguyen <thong.nguyen@bluesg.com.sg>
2025-07-29 15:20:43 +0700

    test: refactor test suites

--- src/game.ts
+++ src/game.ts
@@ -1,8 +1,12 @@
 import { PinError } from "./errors/PinError";
 import { GameInterface } from "./game.interface";
 
+type Frame = number[];
+
 export class Game implements GameInterface {
-  knockedPins = 0;
+  private knockedPins = 0;
+
+  private rollFrames: Frame[] = [];
 
   roll(pins: number): void {
     if (pins < 0) {
@@ -13,9 +17,21 @@ export class Game implements GameInterface {
       throw new PinError("Pins is more than 10");
     }
 
+    // const curFrame = (this.rollFrames[this.rollFrames.length - 1] ??= []);
+
+    // const prevScore = this.rollFrames[this.rollFrames.length - 1].reduce(
+    //   (prev, next) => prev + next,
+    //   0
+    // );
+
     if (pins + this.knockedPins > 10) {
       throw new PinError("Pins is more than 10");
     }
+
+    // curFrame.push(pins);
+    // if (pins === 10) {
+    //   this.rollFrames.push(0);
+    // }
 
     this.knockedPins += pins;
   }
--- test/game.spec.ts
+++ test/game.spec.ts
@@ -9,7 +9,7 @@ describe("Game", () => {
     game = new Game();
   });
 
-  describe("roll", () => {
+  describe("ValidateInput", () => {
     it("should be failed when pin is negative", () => {
       expect(() => game.roll(-1)).toThrow(PinError);
     });
@@ -17,20 +17,13 @@ describe("Game", () => {
     it("should be failed when pin is over 10", () => {
       expect(() => game.roll(11)).toThrow(PinError);
     });
-
-    it("should be failed when knocked down pins in one frame is over 10", () => {
-      game.roll(4);
-      expect(() => game.roll(8)).toThrow();
-    });
   });
 
-  describe("score", () => {
+  describe("NormalGame", () => {
     it("should return 0 in the start of the game", () => {
       expect(game.score()).toBe(0);
     });
-  });
 
-  describe("Behavior", () => {
     it("should return a score according to knocked down pins", () => {
       game.roll(1);
       expect(game.score()).toEqual(1);
@@ -40,6 +33,25 @@ describe("Game", () => {
       game.roll(1);
       game.roll(3);
       expect(game.score()).toEqual(4);
+    });
+
+    it("should be failed when knocked down pins in one frame is over 10", () => {
+      game.roll(4);
+      expect(() => game.roll(8)).toThrow();
+    });
+
+    it("should be failed when knocked down pins is more than 10 in 3 rolls", () => {
+      game.roll(4);
+      game.roll(5);
+      expect(() => game.roll(3)).toThrow();
+    });
+
+    it.skip("should return a score plus spare bonus", () => {
+      game.roll(4);
+      game.roll(6);
+      game.roll(7);
+
+      expect(game.score()).toBe(24);
     });
   });
 });
